4bafd60076422809d7705be47375903a2e2dc12f
Achmad Farhan <36761061+hanfx@users.noreply.github.com>
2023-12-04 15:33:14 +0800

    .

---
+++ functions.py
@@ -0,0 +1,14 @@
+
+# def greet(name, time):
+#     print(f"Good {time}, {name}, hope you are well")
+# greet("shaun", "morning")
+
+from datetime import datetime
+
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+def greet():
+    name = input("Enter ur name: ") #isi nama kamu
+    greetings = input("Enter greetings: ")
+    print(f"Hi, ", name ," it's ",current_time,"," , greetings)
+greet()
