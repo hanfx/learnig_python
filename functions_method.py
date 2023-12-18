15290fbeda9dbf0aebb75e29354a2223efb24fa8
Achmad Farhan <36761061+hanfx@users.noreply.github.com>
2023-12-18 23:42:12 +0800

    Create functions_method.py

---
+++ functions_method.py
@@ -0,0 +1,20 @@
+#functions
+# print('a value')
+# print(input('asl for a value: '))
+
+#methods -> functions of datatypes
+# print('value'.upper())
+# print('value'.lower())
+# print('value'.replace())
+
+# input("ask for value = ")
+# x = input("isi disini: ")
+# print(x.replace('e', '3'))
+
+#new functions
+print(abs(-1.5))
+print(max(11,9))
+print(min(1,8))
+print(len('test'))
+
+
