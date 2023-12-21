4c63c7276158f3755a9facacb234d653fe86d72f
Farhan <36761061+hanfx@users.noreply.github.com>
2023-12-21 12:36:07 +0800

    tambahkan pilihan hitung untuk rumus lain

--- functions_method.py
+++ functions_method.py
@@ -21,41 +21,53 @@
 #pythagoean theorem fucntion
 def calc_pythagorean():
     print("welcome to calculator pythagoras!")
-    pilihan_hitung = str(input("ingin menghitung sisi apa? [A, B, atau C]"))
+    pilihan_hitung = input("ingin menghitung sisi apa? [A, B, atau C] ")
     
-    while pilihan_hitung == "C || c":
+    if pilihan_hitung == "C": #hitung sisi miring
         side_a = int(input("Side A = "))
         side_b = int(input("Side B = "))
         x  = (side_a ** 2) + (side_b ** 2)
         square_root = x ** (0.5)
-        print("Jadi sisi C =", round(square_root,2))
-        hitung_ulang1 = input("Ingin menghitung ulang?: [Y/N]")
-        if hitung_ulang1 == "y":
-            calc_pythagorean()
-        else:
-            print("thank you!")
-            exit()
+        print("Jadi sisi C =", square_root)
+        # hitung_ulang1 = input("Ingin menghitung ulang?: [Y/N]")
+        # if hitung_ulang1 == "y":
+        #     calc_pythagorean()
+        # else:
+        #     print("thank you!")
 
-    while pilihan_hitung == "A" or "a":
+    elif pilihan_hitung == "A": #hitung sisi tegak
         side_b = int(input("Side B = "))
         side_c = int(input("Side C = "))
         x  = (side_b ** 2) - (side_c ** 2)
         square_root = x ** (0.5)
-        print("Jadi sisi A =", round(square_root,2))
+        print("Jadi sisi A =", square_root)
+        # hitung_ulang1 = input("Ingin menghitung ulang?: [Y/N]")
+        # if hitung_ulang1 == "y":
+        #     calc_pythagorean()
+        # else:
+        #     print("thank you!")
 
-    while pilihan_hitung == "B" or "b":
-        side_a = int(input("Side a = "))
-        side_c = int(input("Side c = "))
-        x  = (side_b ** 2) - (side_c ** 2)
+    elif pilihan_hitung == "B": #hitung side B (alas)
+        side_a = int(input("Side A = "))
+        side_c = int(input("Side C = "))
+        x  = (side_c ** 2) - (side_a ** 2)
         square_root = x ** (0.5)
-        print("Jadi sisi B =", round(square_root,2))
+        print("Jadi sisi B =", square_root)
+        # hitung_ulang1 = input("Ingin menghitung ulang?: [Y/N]")
+        # if hitung_ulang1 == "y":
+        #     calc_pythagorean()
+        # else:
+        #     print("thank you!")
 
     else:
-        hitung_ulang = input("Ingin menghitung ulang1?: [Y/N]")
-        if hitung_ulang == "y":
-            calc_pythagorean()
-        else:
-            print("thank you!")
-            exit()
+        print("Mohon inputkan A, B, atau C")
+
+
+    hitung_ulang = input("Ingin menghitung ulang1?: [Y/N]")
+    if hitung_ulang == "y":
+        calc_pythagorean()
+    else:
+        print("Thank you!")
+        exit()
         
 calc_pythagorean()
