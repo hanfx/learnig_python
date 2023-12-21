ea9794eb9862774cd9cfecb7bf1b507837817c26
Achmad Farhan <36761061+hanfx@users.noreply.github.com>
2023-12-21 10:43:04 +0800

    Update functions_method.py

--- functions_method.py
+++ functions_method.py
@@ -18,10 +18,44 @@
 # print(min(1,8))
 # print(len('test'))
 
+#pythagoean theorem fucntion
+def calc_pythagorean():
+    print("welcome to calculator pythagoras!")
+    pilihan_hitung = str(input("ingin menghitung sisi apa? [A, B, atau C]"))
+    
+    while pilihan_hitung == "C || c":
+        side_a = int(input("Side A = "))
+        side_b = int(input("Side B = "))
+        x  = (side_a ** 2) + (side_b ** 2)
+        square_root = x ** (0.5)
+        print("Jadi sisi C =", round(square_root,2))
+        hitung_ulang1 = input("Ingin menghitung ulang?: [Y/N]")
+        if hitung_ulang1 == "y":
+            calc_pythagorean()
+        else:
+            print("thank you!")
+            exit()
 
-#pythagoean theorem
-side_b = int(input("Side B = "))
-side_a = int(input("Side A = "))
-x  = (side_a ** 2) + (side_b ** 2)
-square_root = x ** (0.5)
-print("So the hypotenuse =", round(square_root,2))
+    while pilihan_hitung == "A" or "a":
+        side_b = int(input("Side B = "))
+        side_c = int(input("Side C = "))
+        x  = (side_b ** 2) - (side_c ** 2)
+        square_root = x ** (0.5)
+        print("Jadi sisi A =", round(square_root,2))
+
+    while pilihan_hitung == "B" or "b":
+        side_a = int(input("Side a = "))
+        side_c = int(input("Side c = "))
+        x  = (side_b ** 2) - (side_c ** 2)
+        square_root = x ** (0.5)
+        print("Jadi sisi B =", round(square_root,2))
+
+    else:
+        hitung_ulang = input("Ingin menghitung ulang1?: [Y/N]")
+        if hitung_ulang == "y":
+            calc_pythagorean()
+        else:
+            print("thank you!")
+            exit()
+        
+calc_pythagorean()
