0e54e45ac39ccb88dda1a6c734239cb5cd9fda14
Farhan <36761061+hanfx@users.noreply.github.com>
2024-03-14 11:48:13 +0800

    Update control_flow.py

--- control_flow.py
+++ control_flow.py
@@ -11,7 +11,26 @@
 # print("loop has finished")
 
 #for loop
-test_list = {1:2, 5:3, 4:8}
-for k,v in test_list.items():
-    print(k)
-    print(v)
+# test_list = {1:2, 5:3, 4:8}
+# for k,v in test_list.items():
+#     print(k)
+#     print(v)
+
+#truthy and falsy
+# if 0:
+#     print("truthy")
+# else:
+#     print("falsy")
+
+#exercise
+list = [1, 2, 3, 4, 5]
+hitung_list = 0
+for x in list:
+    if x == 2:
+        print('the value is 2')
+    else:
+        print('the value is not 2')
+
+    while hitung_list <= 6:
+        print('item terakhir')
+        hitung_list += 1
